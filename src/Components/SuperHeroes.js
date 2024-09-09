f15889e17dec208a90c30268e0b63aaa15fe1934
SymuNadeem <msymunnadeem@gmail.com>
2024-09-09 11:42:09 +0600

    updated quary

--- src/Components/SuperHeroes.js
+++ src/Components/SuperHeroes.js
@@ -2,38 +2,29 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const SuperHeroes = () => {
-    const [loading,setLoading]=useState(true)
-    const [sdata,setSdata]=useState([])
-    const [error,seterror]= useState('')
+  const [loading, setLoading] = useState(true)
+   const [data, setData] = useState([])
 
-    useEffect(()=>{
-       
-      axios.get("http://localhost:5000/superheroes")
-      .then((res)=>{
+   useEffect(()=>{
+       axios.get("http://localhost:5000/superheroes")
+       .then((res)=>{
+        setData(res.data)
         setLoading(false)
-        setSdata(res.data)
-      })
-    .catch((err)=>{
-      seterror(err);
-      setLoading(false)
-    })
-    },[])
-
-    if(loading){
-      return <div>Loading....</div>
-    }
-
-    if(error){
-      return <div>{error.message}</div>
-    }
-
+       })
+   },[])
+ 
+   if(loading){
+    return <div>Loading....</div>
+   }
 
   return (
     <div>
-        <h1>Super Heroes</h1>
-        {sdata.map((item)=>{
-          return <div>{item.name}</div>
-        })}
+        <h1>Traditional Data fetching</h1>
+        {data.map((item)=>(
+          <div key={item.id}>
+              <h3>{item.name}</h3>
+          </div>
+        ))}
     </div>
   );
 };
